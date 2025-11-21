e8836d6964f6569803b4a97c646e9d75774d47c0
Aya Ridene <rideneaya100@gmail.com>
2025-11-21 18:15:47 +0100

    update front

--- AppBeta/App.js
+++ AppBeta/App.js
@@ -4,13 +4,15 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "./screens/Login";
 import Register from "./screens/Register";
 import ProfileEdit from "./screens/ProfileEdit";
+import Splash from "./screens/Splash";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: true }}>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        
         <Stack.Screen name="Login" component={Login} />
         <Stack.Screen name="Register" component={Register} />
         <Stack.Screen name="ProfileEdit" component={ProfileEdit} />
